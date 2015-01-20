af9ba2bfa7a6841538b5c882bac0525b3669ec76
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-01-20 07:48:45 -0300

    ADD other sipreco modules

---
+++ addons/account_payment_voucher/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{'active': False,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'category': 'Accounting & Finance',
+    'data': [
+        'account_payment_view.xml',
+        'account_voucher_view.xml',
+    ],
+    'demo': [],
+    'depends': [
+        'account_payment',
+        ],
+    'description': '''
+Account Payment Voucher Create
+==============================
+On confirmation of an account payment order, create vouchers
+''',
+    'installable': True,
+    'name': 'Account Payment Voucher Create',
+    'test': [],
+    'version': '1.243'}
