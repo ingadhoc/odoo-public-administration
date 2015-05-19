41e89ed1add8e67c185b7cf7700cb2369b981fa5
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-05-19 12:12:32 -0300

    FIX __openerp__ file

--- addons/sipreco_public_budget/__openerp__.py
+++ addons/sipreco_public_budget/__openerp__.py
@@ -9,11 +9,8 @@
         'views/custom_views.xml',
         'views/account_asset_view.xml',
         'views/account_voucher_view.xml',
-<<<<<<< HEAD
         'views/account_check_view.xml',
-=======
         'workflow/account_voucher_workflow.xml',
->>>>>>> juan_dev
         'security/security.xml',
         'data/data.xml',
         'reports/receipt_report.xml',
