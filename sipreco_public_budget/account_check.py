c2b82bb0f84deb561ff08b410b83e4d20f9adf7b
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-20 18:59:48 -0300

    IMP format

--- sipreco_public_budget/account_check.py
+++ sipreco_public_budget/account_check.py
@@ -27,15 +27,25 @@ class account_check(models.Model):
     @api.multi
     def check_check_cancellation(self):
         for check in self:
-            if check.type == 'issue' and check.state not in ['draft', 'to_be_handed', 'handed']:
+            if check.type == 'issue' and check.state not in [
+                    'draft', 'to_be_handed', 'handed']:
                 raise Warning(_(
-                    'You can not cancel issue checks in states other than "draft", "to be handed" or "handed". First try to change check state.'))
+                    'You can not cancel issue checks in states other than\
+                    "draft", "to be handed" or "handed".\
+                    First try to change check state.'))
             # third checks received
-            elif check.type == 'third' and not self.third_handed_voucher_id and check.state not in ['draft', 'holding']:
+            elif check.type == 'third' and (
+                    not self.third_handed_voucher_id) and (
+                    check.state not in ['draft', 'holding']):
                 raise Warning(_(
-                    'You can not cancel received third checks in states other than "draft or "holding". First try to change check state.'))
+                    'You can not cancel received third checks in states other\
+                    than "draft or "holding".\
+                    First try to change check state.'))
             # third checks handed
-            elif check.type == 'third' and self.third_handed_voucher_id and check.state not in ['draft', 'holding']:
+            elif check.type == 'third' and (
+                    self.third_handed_voucher_id) and (
+                    check.state not in ['draft', 'holding']):
                 raise Warning(_(
-                    'You can not cancel handed third checks in states other than "handed". First try to change check state.'))
+                    'You can not cancel handed third checks in states other\
+                    than "handed". First try to change check state.'))
         return True
