5f03de95a15b20a2b5d9d426a0731d2bc6e78ec2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-09-09 10:23:30 -0300

    IMP move tomodels folder

---
+++ sipreco_public_budget/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import partner
+import transaction_type
+import account_voucher
+import account_check
+import budget
+import definitive_line
+import preventive_line
+import transcation
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
