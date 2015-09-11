f37d2dbd2ce7bc0be596c5dd8f0a4516415c6104
gonzaloruzafa <gr@ingadhoc.com>
2015-09-11 09:31:15 -0300

    ADD Sipreco Setup data CMD

---
+++ sipreco_setup_data_cmd/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sipreco Set Up Data CMD',
+    'version': '8.0.0.0.0',
+    'category': 'Accounting',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Sipreco Set Up Data TMC
+=======================
+    """,
+    'author':  'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'images': [
+    ],
+    'depends': [
+        'sipreco_public_budget',
+        'l10n_ar_bank',
+        'l10n_ar_states',
+    ],
+    'data': [
+        'data/location.xml',
+        'data/budget_position.xml',
+        'data/res_company.xml',
+        'data/res_users.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
