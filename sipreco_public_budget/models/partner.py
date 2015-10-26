ada69f3fd942a0a9995a37631505c3995d73268a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-10-26 18:54:54 -0300

    IMP make partner document number unique

--- sipreco_public_budget/models/partner.py
+++ sipreco_public_budget/models/partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api, _
 
 
 class partner(models.Model):
@@ -20,4 +20,20 @@ class partner(models.Model):
     document_number = fields.Char(
         required=True,
         )
+
+    @api.one
+    @api.constrains('subsidy_recipient', 'document_number')
+    def check_unique_document_number(self):
+        if self.subsidy_recipient and self.document_number:
+            same_document_partners = self.search([
+                ('document_number', '=', self.document_number),
+                ('subsidy_recipient', '=', True),
+                ('id', '!=', self.id),
+                ])
+            if same_document_partners:
+                raise Warning(_(
+                    'El número de documento debe ser único por receptor de '
+                    'subsidio!\n'
+                    '* ids de receptores: %s') % same_document_partners.ids)
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
