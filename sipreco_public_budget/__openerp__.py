935f821a59252403978ae77633de165d2358fb6f
Juan José Scarafía <scarafia.juanjose@gmail.com>
2015-10-29 11:03:14 -0300

    Update __openerp__.py

--- sipreco_public_budget/__openerp__.py
+++ sipreco_public_budget/__openerp__.py
@@ -104,4 +104,4 @@ Customizaciones especificas al modulo public_budget para SIPRECO
     'installable': True,
     'name': 'Public Budget Sipreco Customizations',
     'test': [],
-    'version': '8.0.1.6.0'}
+    'version': '8.0.1.7.0'}
