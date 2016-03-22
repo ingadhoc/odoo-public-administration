ffd748e2e45c1d14466649af4cdcd6b21fa6a6f5
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-03-22 10:31:42 -0300

    ADD print button in budget to date

--- public_budget/__openerp__.py
+++ public_budget/__openerp__.py
@@ -63,6 +63,6 @@ Modulo generico para gestion publica presupuestaria/contable
     'license': 'AGPL-3',
     'name': 'Public Budget',
     'test': [],
-    'version': '8.0.0.9.0',
+    'version': '8.0.0.10.0',
     'website': ''}
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- sipreco_public_budget/__openerp__.py
+++ sipreco_public_budget/__openerp__.py
@@ -108,4 +108,4 @@ Customizaciones especificas al modulo public_budget para SIPRECO
     'installable': True,
     'name': 'Public Budget Sipreco Customizations',
     'test': [],
-    'version': '8.0.1.11.1'}
+    'version': '8.0.1.12.1'}
