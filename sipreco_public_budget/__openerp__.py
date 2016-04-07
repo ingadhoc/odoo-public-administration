433c6ec33671d47b276d4b2dda8aba597f6c9dce
gonzaloruzafa <gr@ingadhoc.com>
2016-04-07 11:29:58 -0300

    FIX Version

--- sipreco_public_budget/__openerp__.py
+++ sipreco_public_budget/__openerp__.py
@@ -109,4 +109,4 @@ Customizaciones especificas al modulo public_budget para SIPRECO
     'installable': True,
     'name': 'Public Budget Sipreco Customizations',
     'test': [],
-    'version': '8.0.1.13.1'}
+    'version': '8.0.1.14.1'}
