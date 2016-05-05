86d36f92927503f3c7a0c5b67a7df4ec95984cdb
Gonzalo Ruzafa <gruzafa@ingenieriaadhoc.com>
2016-05-05 16:45:34 -0300

    Update __openerp__.py

--- sipreco_public_budget/__openerp__.py
+++ sipreco_public_budget/__openerp__.py
@@ -110,4 +110,4 @@ Customizaciones especificas al modulo public_budget para SIPRECO
     'installable': True,
     'name': 'Public Budget Sipreco Customizations',
     'test': [],
-    'version': '8.0.1.16.0'}
+    'version': '8.0.1.17.0'}
