9cd97389a78848464e1b9026450e5f4336c62d98
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-07-19 10:24:20 -0300

    IMP

--- sipreco_subsidy_management/models/__init__.py
+++ sipreco_subsidy_management/models/__init__.py
@@ -4,4 +4,5 @@ import approval_arrangement
 import subsidy_rendition
 import subsidy_claim
 import transaction_type
+import account_voucher
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- sipreco_subsidy_management/models/subsidy.py
+++ sipreco_subsidy_management/models/subsidy.py
@@ -2,7 +2,7 @@
 from openerp import fields, models, api
 from openerp.exceptions import Warning
 from dateutil.relativedelta import relativedelta
-from datetime import date
+# from datetime import date
 import openerp.addons.decimal_precision as dp
 import logging
 _logger = logging.getLogger(__name__)
@@ -44,8 +44,8 @@ class PublicBudgetSubsidy(models.Model):
     parliamentary_expedient = fields.Char(
         'Expediente Parlamentario',
     )
-    charge_date = fields.Date(
-        compute='get_charge_date',
+    cargo_date = fields.Date(
+        compute='get_cargo_data',
         string='Fecha del Cargo',
     )
     dispositional_order = fields.Char(
@@ -53,21 +53,25 @@ class PublicBudgetSubsidy(models.Model):
         readonly=True,
     )
     accountability_state = fields.Selection([
-        ('charge_made', 'Cargo Efectuado'),
-        ('rendition_presented', 'Rendición Presentada'),
-        ('rendition_approved', 'Rendición Aprobada'),
+        ('pending', 'Cargos Pendientes'),
+        ('approved', 'Aprobada'),
     ],
         'Estado de la Rendición',
+        compute='get_state',
+        store=True,
     )
-    accountability_overcome = fields.Boolean(
-        compute='_get_accountability_expiry_date',
-        search='search_accountability_overcome',
-        string='Rendición Vencida?',
-    )
+    # accountability_state = fields.Selection([
+    #     ('charge_made', 'Cargo Efectuado'),
+    #     ('rendition_presented', 'Rendición Presentada'),
+    #     ('rendition_approved', 'Rendición Aprobada'),
+    # ],
+    #     'Estado de la Rendición',
+    # )
     accountability_expiry_date = fields.Date(
-        compute='_get_accountability_expiry_date',
+        compute='get_cargo_data',
         string='Vencimiento de Rendición ',
         help='Fecha de vencimiento de presentación de rendición',
+        store=True,
     )
     accountability_administrative_expedient_id = fields.Many2one(
         'public_budget.expedient',
@@ -92,18 +96,39 @@ class PublicBudgetSubsidy(models.Model):
         digits=dp.get_precision('Account'),
         # states={'closed': [('readonly', True)]},
     )
-    rendition_amount = fields.Float(
-        'Monto Rendido',
+    cargo_amount = fields.Float(
+        'Cargos',
+        help='Cargos Efectuados',
+        compute='get_cargo_data',
+        store=True,
+    )
+    pendientes_rendicion_amount = fields.Float(
+        'Pendiente Rendición',
+        help='Cargos Pendientes de Rendición',
+        compute='get_cargo_data',
+        store=True,
+    )
+    pendientes_aprobacion_amount = fields.Float(
+        'Pendiente Aprobación',
+        help='Cargos Pendientes de Aprobación',
+        compute='get_cargo_data',
+        store=True,
+    )
+    rendido_amount = fields.Float(
+        'Rendido',
+        help='Rendiciones Presentadas',
         compute='get_amounts',
         store=True,
     )
-    approved_amount = fields.Float(
-        'Monto Aprobado',
+    aprobado_amount = fields.Float(
+        'Aprobado',
+        help='Rendiciones Aprobadas',
         compute='get_amounts',
         store=True,
     )
-    pending_amount = fields.Float(
-        'Monto Pendiente',
+    revision_amount = fields.Float(
+        'En Revisión',
+        help='Rendiciones presentadas en Revisión',
         compute='get_amounts',
         store=True,
     )
@@ -114,50 +139,52 @@ class PublicBudgetSubsidy(models.Model):
         'rendition_ids.approved_amount',
     )
     def get_amounts(self):
-        rendition_amount = sum(
+        rendido_amount = sum(
             self.rendition_ids.mapped('rendition_amount'))
-        approved_amount = sum(
+        aprobado_amount = sum(
             self.rendition_ids.mapped('approved_amount'))
-        self.approved_amount = approved_amount
-        self.rendition_amount = rendition_amount
-        self.pending_amount = rendition_amount - approved_amount
+        self.aprobado_amount = aprobado_amount
+        self.rendido_amount = rendido_amount
+        self.revision_amount = rendido_amount - aprobado_amount
 
-    @api.model
-    def search_accountability_overcome(self, operator, value):
-        # 30 dias para atras, solo hábiles
-        to_date = date.today()
-        business_days_to_add = 30
-        while business_days_to_add > 0:
-            to_date = to_date + relativedelta(days=-1)
-            weekday = to_date.weekday()
-            if weekday >= 5:    # sunday = 6
-                continue
-            # if to_date in holidays:
-            #     continue
-            business_days_to_add -= 1
-
-        subsidy_transactions = self.search([]).mapped('transaction_id')
-        overcome_checks = self.env['account.check'].search([
-            ('voucher_id.transaction_id', 'in', subsidy_transactions.ids),
-            ('handed_date', '<', fields.Date.to_string(to_date)),
-        ], limit=1)
-
-        transaction_ids = overcome_checks.mapped(
-            'voucher_id.transaction_id.id')
-
-        # TODO check that value should be True
-        if operator == '=':
-            operator = 'in'
-        elif operator == '!=':
-            operator = 'not in'
-        else:
-            return []
-        return [('transaction_id', 'in', transaction_ids)]
+    @api.onchange('expedient_id', 'partner_id')
+    def set_subsidy_name(self):
+        self.name = '%s - %s' % (
+            self.expedient_id.number or '', self.partner_id.name or '')
 
     @api.one
-    def _get_accountability_expiry_date(self):
-        if self.charge_date:
-            expiry_date = fields.Date.from_string(self.charge_date)
+    @api.depends(
+        'pendientes_aprobacion_amount',
+    )
+    def get_state(self):
+        if self.pendientes_aprobacion_amount:
+            accountability_state = 'pending'
+        else:
+            accountability_state = 'approved'
+        self.accountability_state = accountability_state
+
+    @api.one
+    @api.depends(
+        'rendido_amount',
+        'aprobado_amount',
+        'voucher_ids.cargo_date',
+        'voucher_ids.cargo_amount',
+        # TODO chequear si hace falta esto o no
+        'advance_voucher_ids.cargo_date',
+        'advance_voucher_ids.cargo_amount',
+    )
+    def get_cargo_data(self):
+        vouchers = self.voucher_ids + self.advance_voucher_ids
+        cargo_amount = sum(vouchers.mapped('cargo_amount'))
+        cargo_date = vouchers.search([
+            ('id', 'in', vouchers.ids),
+            ('cargo_date', '!=', False),
+        ], order='cargo_date desc', limit=1).cargo_date
+
+        expiry_date = False
+        if cargo_date:
+            expiry_date = fields.Date.from_string(cargo_date)
+            # TODO, parametrizable?
             business_days_to_add = 30
             while business_days_to_add > 0:
                 expiry_date = expiry_date + relativedelta(days=+1)
@@ -167,22 +194,11 @@ class PublicBudgetSubsidy(models.Model):
                 # if to_date in holidays:
                 #     continue
                 business_days_to_add -= 1
-            # TODO chequear que no hace falta convertir
-            self.accountability_expiry_date = fields.Date.to_string(
-                expiry_date)
-            print '(date.today() - expiry_date)', (date.today() - expiry_date)
-            if date.today() > expiry_date:
-                self.accountability_overcome = True
-
-    @api.onchange('expedient_id', 'partner_id')
-    def set_subsidy_name(self):
-        self.name = '%s - %s' % (
-            self.expedient_id.number or '', self.partner_id.name or '')
-
-    @api.one
-    def get_charge_date(self):
-        # issued_checks = self.advance_voucher_ids.mapped('issued_check_ids')
-        last_check = self.env['account.check'].search([
-            ('voucher_id.transaction_id', '=', self.transaction_id.id),
-        ], order='handed_date desc', limit=1)
-        self.charge_date = last_check.handed_date
+        self.cargo_date = cargo_date
+        self.accountability_expiry_date = fields.Date.to_string(
+            expiry_date)
+        self.cargo_amount = cargo_amount
+        self.pendientes_rendicion_amount = (
+            cargo_amount - self.rendido_amount)
+        self.pendientes_aprobacion_amount = (
+            cargo_amount - self.aprobado_amount)
