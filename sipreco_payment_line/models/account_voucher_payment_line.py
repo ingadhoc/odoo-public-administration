3abb622057c9c2e6e3db12c5e899bed08ee7269f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-10-14 16:53:49 -0300

    ADD sipreco payment line

---
+++ sipreco_payment_line/models/account_voucher_payment_line.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from openerp import api, models, fields, _
+from openerp.exceptions import Warning as UserError
+from openerp.addons.decimal_precision import decimal_precision as dp
+import logging
+
+_logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class AccountVoucherPaymentLine(models.Model):
+    """Extend model account.bank.statement."""
+    _name = 'account.voucher.payment_line'
+    # _name = 'account_voucher_payment_line_import'
+    _description = 'Account Vouchers Payment Lines'
+
+    voucher_id = fields.Many2one(
+        'account.voucher',
+        'Voucher',
+        required=True,
+        ondelete='cascade'
+    )
+    partner_id = fields.Many2one(
+        'res.partner',
+        'Partner',
+        required=True,
+    )
+    cuit = fields.Char(
+    )
+    bank_account_id = fields.Many2one(
+        'res.partner.bank',
+        string='Bank Account',
+        domain="[('partner_id', '=', partner_id)]",
+    )
+    amount = fields.Float(
+        digits=dp.get_precision('Account'),
+    )
+
+    @api.multi
+    def _get_linea_archivo_banco(self):
+        def only_digits(string):
+            return filter(lambda x: x.isdigit(), string)
+
+        self.ensure_one()
+        # if not self.partner_id.numero_legajo:
+        #     UserError(_('El partner %s no tiene número de legajo'))
+        if (
+                not self.bank_account_id.acc_number or
+                not self.bank_account_id.numero_de_sucursal):
+            raise UserError(_(
+                'Se requiere cuenta bancaria con número de sucursal.\n'
+                '* Partner: %s') % (self.partner_id.name))
+
+        voucher = self.voucher_id
+
+        Registro = ""
+
+        # Right("00" & CLng(Sucursal), 2)
+        Registro += voucher.sucursal_de_cuenta_debito.rjust(2, '0')
+
+        # Right("00000000" & CLng(Cuenta), 8)
+        Registro += only_digits(self.bank_account_id.acc_number).rjust(8, '0')
+
+        Registro += "62"
+
+        # Right("000000" & CLng(Legajo), 6)
+        Registro += (
+            only_digits(self.partner_id.numero_legajo) or '').rjust(6, '0')
+
+        # Right("00000000000000" & CLng(Importe * 100), 14)
+        Registro += str(int(self.amount * 100)).rjust(14, '0')
+
+        # Format(Range("E5").Text, "YYYYMMDD")
+        Registro += fields.Date.from_string(
+            voucher.fecha_de_acreditacion).strftime('%Y%m%d')
+
+        Registro += "00"
+
+        Registro += "2"
+
+        Registro += "000000000000"
+
+        # Right("0" & Range("E7").Value, 1)
+        Registro += voucher.tipo_de_pago.rjust(1, '0')
+
+        # Right("000" & Range("E6").Value, 3)
+        Registro += voucher.grupo_asingado_por_bmr.rjust(3, '0')
+
+        Registro += "     "
+
+        Registro += "00000"
+
+        # Right("00000" & Range("E10").Value, 5)
+        Registro += voucher.tipo_de_cuenta.rjust(5, '0')
+
+        # Right("00" & Range("E8").Value, 2)
+        Registro += voucher.sucursal_de_cuenta_debito.rjust(2, '0')
+
+        # Right("00000000" & Range("E9").Value, 8)
+        Registro += voucher.numero_de_cuenta_debito.rjust(8, '0')
+
+        return Registro
---
+++ sipreco_payment_line/wizards/account_voucher_payment_line_import.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+import logging
+import base64
+from StringIO import StringIO
+from openerp import api, models, fields, _
+from openerp.exceptions import Warning as UserError
+
+_logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class AccountVoucherPaymentLineImport(models.TransientModel):
+    _name = 'account.voucher.payment_line.import'
+    _description = 'Account Vouchers Payment Lines Import'
+
+    data_file = fields.Binary(
+        'Archivo de Líneas de Pago',
+        required=True,
+    )
+
+    @api.multi
+    def import_file(self):
+        self.ensure_one()
+        data_file = base64.b64decode(self.data_file)
+        self._import_file(data_file)
+
+    @api.model
+    def _import_file(self, data_file):
+        payment_lines = self.env['account.voucher.payment_line']
+        payment_lines_vals = self._parse_file(data_file)
+        self._check_parsed_data(payment_lines_vals)
+        for payment_line_vals in payment_lines_vals:
+            payment_lines += self._import_payment_lines(payment_line_vals)
+        return payment_lines
+
+    @api.model
+    def _import_payment_lines(self, payment_line_vals):
+        cuit = payment_line_vals.pop('cuit')
+        partner = self._find_partner(cuit)
+        if not partner and cuit:
+            raise UserError(
+                _('No se pudo encontrar partner para cuit %s.') % cuit
+            )
+
+        active_model = self._context.get('active_model')
+        active_id = self._context.get('active_id')
+        if active_model != 'account.voucher':
+            raise UserError(
+                _('No se está ejecutando el asistente desde un pago.')
+            )
+        if not active_id:
+            raise UserError(
+                _('No se encontró un pago activo en el contexto.')
+            )
+        voucher = self.env[active_model].browse(active_id)
+        # check if line already exist
+        vals = {
+            'cuit': cuit,
+            'partner_id': partner.id,
+            'bank_account_id': (
+                partner.bank_ids and partner.bank_ids[0].id or False),
+            'voucher_id': voucher.id,
+            'amount': payment_line_vals.get('amount'),
+        }
+        payment_line = self.env['account.voucher.payment_line'].search(
+            [('cuit', '=', cuit), ('voucher_id', '=', voucher.id)], limit=1)
+        if payment_line:
+            payment_line.write(vals)
+        else:
+            payment_line = payment_line.create(vals)
+        return payment_line
+
+    @api.model
+    def _parse_file(self, data_file):
+        res = []
+        try:
+            # skip first line
+            for line in StringIO(data_file).readlines()[1:]:
+                _logger.info('Parsing line "%s"' % line)
+                line_vals = line.strip().split()
+                try:
+                    float(line_vals[4])
+                except ValueError:
+                    _logger.warning(
+                        "Could not get value from line %s" % line_vals)
+                res.append({
+                    'cuit': line_vals[1],
+                    'amount': float(line_vals[4]),
+                })
+        except Exception, e:
+            raise UserError(_(
+                "Ocurrió un error al importar. "
+                "El archivo puede no ser valido.\n\n %s" % e.message
+            ))
+        return res
+
+    @api.model
+    def _check_parsed_data(self, payment_lines):
+        # pylint: disable=no-self-use
+        """ Basic and structural verifications """
+        if len(payment_lines) == 0:
+            raise UserError(_('This file doesn\'t contain any line.'))
+
+    @api.model
+    def _find_partner(self, cuit):
+        return self.env['res.partner'].search([
+            ('document_type_id.afip_code', '=', 80),
+            ('document_number', '=', cuit),
+        ], limit=1)
