d2984d9a5514f6c2a07e790ae702949866f8344d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-10 11:11:01 -0300

    IMP version

--- sipreco_setup_data_cmd/__openerp__.py
+++ sipreco_setup_data_cmd/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Sipreco Set Up Data CMD',
-    'version': '8.0.0.3.0',
+    'version': '8.0.0.4.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
