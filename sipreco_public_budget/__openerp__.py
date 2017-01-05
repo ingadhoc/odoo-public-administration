29d02098e6f12863353605f2ef89a557e8b06204
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-01-05 12:36:48 -0300

    REF

--- sipreco_public_budget/__openerp__.py
+++ sipreco_public_budget/__openerp__.py
@@ -115,4 +115,4 @@ Customizaciones especificas al modulo public_budget para SIPRECO
     'installable': True,
     'name': 'Public Budget Sipreco Customizations',
     'test': [],
-    'version': '8.0.1.22.0'}
+    'version': '8.0.1.23.0'}
