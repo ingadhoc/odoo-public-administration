dd3ed5e569df653f012b1e046d006182cdb4ef74
Juan José Scarafía <jjs@adhoc.com.ar>
2017-05-14 00:27:18 +0200

    9.0 mig reports (#147)
    * REF clean
    
    * MIG reports

--- public_budget/wizard/__init__.py
+++ public_budget/wizard/__init__.py
@@ -3,5 +3,3 @@ from . import transaction_definitive_make_invoice
 from . import transaction_definitive_mass_invoice_create
 from . import budget_analysis_wizard
 from . import avance_request_report_wizard
-# TODO enable
-# from . import account_check_handed_wizard
