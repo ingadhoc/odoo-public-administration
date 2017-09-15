045958ce8e7b327fcd7916eed3336923b7667e1d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-09-15 08:46:27 -0300

    ADD change check

--- public_budget/__openerp__.py
+++ public_budget/__openerp__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Public Budget',
     'license': 'AGPL-3',
-    'version': '9.0.1.2.0',
+    'version': '9.0.1.3.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'category': 'Accounting & Finance',
--- public_budget/models/account_payment.py
+++ public_budget/models/account_payment.py
@@ -36,3 +36,70 @@ class AccountPayment(models.Model):
         else:
             return super(
                 AccountPayment, self)._compute_destination_account_id()
+
+    @api.multi
+    def confirm_check_change(self):
+        self.ensure_one()
+        replaced_payment_id = self._context.get('replaced_payment_id')
+        if not replaced_payment_id:
+            raise ValidationError(_('No replaced_payment_id on context'))
+        replaced_payment = self.browse(replaced_payment_id)
+        check = replaced_payment.check_id
+        if check.state != 'handed':
+            raise ValidationError(_(
+                "Can't change check!. Check not found or check not in handed"
+                " state"))
+        return_payment = replaced_payment.copy({
+            'payment_date': self.payment_date,
+            'payment_type': 'inbound',
+            'communication': _('Check Return'),
+            'receiptbook_id': replaced_payment.receiptbook_id.id,
+            'document_number': replaced_payment.document_number,
+            'payment_method_id': self.env.ref(
+                'account.account_payment_method_manual_in').id
+        })
+        return_payment.post()
+
+        check._add_operation(
+            'changed', return_payment,
+            partner=return_payment.partner_id, date=return_payment.create_date)
+
+        self.post()
+        (self.move_line_ids + return_payment.move_line_ids).filtered(
+            lambda r: r.account_id == self.destination_account_id).reconcile()
+
+    @api.multi
+    def change_check(self):
+        self.ensure_one()
+        # context = self._context.copy()
+        if self.check_id.state != 'handed':
+            raise ValidationError(_(
+                "Can't change check!. Check not found or check not in handed"
+                " state"))
+        context = {
+            'default_payment_group_company_id': self.company_id.id,
+            'default_payment_type': self.payment_type,
+            'default_partner_id': self.partner_id.id,
+            'default_partner_type': self.partner_type,
+            'payment_group': True,
+            'default_amount': self.amount,
+            # 'tree_view_ref': (
+            #     'account_payment_group.'
+            #     'view_account_payment_from_group_tree'),
+            'default_receiptbook_id': self.receiptbook_id.id,
+            'default_document_number': self.document_number,
+            'default_payment_group_id': self.payment_group_id.id,
+            'replaced_payment_id': self.id,
+        }
+        return {
+            'name': _('Payment Lines'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref(
+                'public_budget.view_account_payment_change_check_form').id,
+            'res_model': 'account.payment',
+            'target': 'new',
+            # 'res_id': self.id,
+            'context': context,
+        }
