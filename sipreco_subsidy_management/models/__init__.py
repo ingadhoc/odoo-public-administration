1beed27e08a210f728032b65b9c5c7b21949132e
nicomacr <nmr@adhoc.com.ar>
2017-10-17 12:02:54 -0300

    [9.0] ADD subsidy resolution

--- sipreco_subsidy_management/models/__init__.py
+++ sipreco_subsidy_management/models/__init__.py
@@ -4,4 +4,5 @@ from . import subsidy_approval_arrangement
 from . import subsidy_rendition
 from . import subsidy_note
 from . import subsidy_note_type
+from . import subsidy_resolution
 from . import transaction_type
---
+++ sipreco_subsidy_management/models/subsidy_resolution.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models, api
+
+
+class PublicBudgetSubsidyResolution(models.Model):
+
+    _name = 'public_budget.subsidy.resolution'
+
+    name = fields.Char(
+        'Nombre',
+        required=True
+    )
+    date = fields.Date(
+        string='Fecha de la Resolucion',
+        required=True
+    )
+    reference = fields.Char(
+        'Referencia',
+        required=True,
+    )
+    expedient_id = fields.Many2one(
+        'public_budget.expedient',
+        string='Expediente',
+        required=True,
+    )
+    state = fields.Selection([
+        ('not_presented', 'No Presentado'),
+        ('presented', 'Presentado')],
+        'Estado',
+        default='not_presented'
+    )
+    subsidy_resolution_line_ids = fields.One2many(
+        'public_budget.subsidy.resolution.line',
+        'subsidy_resolution_id'
+    )
+
+    @api.multi
+    def action_change_state(self):
+        for rec in self:
+            if rec.state == 'not_presented':
+                rec.state = 'presented'
+            elif rec.state == 'presented':
+                rec.state = 'not_presented'
+
+
+class PublicBudgetSubsidyResolutionLines(models.Model):
+
+    _name = 'public_budget.subsidy.resolution.line'
+
+    name = fields.Char(
+        'Nombre de Receptor',
+        required=True
+    )
+    dni = fields.Integer(
+        'DNI de Receptor',
+        required=True
+    )
+    expedient_id = fields.Many2one(
+        'public_budget.expedient',
+        string='Expediente',
+    )
+    partner_id = fields.Many2one(
+        'res.partner',
+        'Consejal',
+        domain=[('employee', '=', True)],
+        required=True,
+    )
+    amount = fields.Integer(
+        'Monto',
+        required=True,
+    )
+    subsidy_resolution_id = fields.Many2one(
+        'public_budget.subsidy.resolution'
+    )
