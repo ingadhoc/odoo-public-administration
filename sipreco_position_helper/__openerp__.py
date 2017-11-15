3beb8310e814ca508a610d521ff249cf5005d65f
Juan José Scarafía <jjs@adhoc.com.ar>
2017-11-14 21:11:05 -0300

    ADD sipreco_position_helper (#166)
    * ADD sipreco_position_helper
    
    * FIX sipreco_
    
    * REF

---
+++ sipreco_position_helper/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'category': 'Accounting & Finance',
+    'data': [
+        'views/budget_position_view.xml',
+    ],
+    'demo': [
+        'demo/public_budget.budget_position.csv',
+    ],
+    'depends': [
+        'public_budget',
+    ],
+    'installable': True,
+    'name': 'Sipreco Position Helper',
+    'test': [],
+    'version': '9.0.1.0.0',
+}
