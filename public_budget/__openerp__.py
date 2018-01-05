5c282da96eef6cf27e28ae79268af49f1814ebb7
nicomacr <nmr@adhoc.com.ar>
2018-01-05 11:47:03 -0300

    9.0 fix expedient report noupdate

--- public_budget/__openerp__.py
+++ public_budget/__openerp__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Public Budget',
     'license': 'AGPL-3',
-    'version': '9.0.1.16.0',
+    'version': '9.0.1.17.0',
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
     'category': 'Accounting & Finance',
