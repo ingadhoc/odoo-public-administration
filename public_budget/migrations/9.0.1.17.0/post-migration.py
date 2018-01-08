5f2712fd0b47a04a41d30a659eb4c9f50e573758
nicomacr <nmr@adhoc.com.ar>
2018-01-08 10:36:03 -0300

    IMP migration script

---
+++ public_budget/migrations/9.0.1.17.0/post-migration.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# from openerp.api import Environment
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    cr = env.cr
+
+    cr.execute("""
+        UPDATE ir_model_data set noupdate=True
+        WHERE name = 'action_aeroo_report_expedient' and module = 'public_budget'
+    """)
