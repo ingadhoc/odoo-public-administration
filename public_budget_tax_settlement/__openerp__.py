c67034a83053556ca3e6af2ecdceee01afec8900
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-03-26 10:21:10 -0300

    [IMP] add public_budget_tax_settlement/

---
+++ public_budget_tax_settlement/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'author': 'ADHOC SA,Odoo Community Association (OCA)',
+    'website': 'www.adhoc.com.ar',
+    'category': 'Accounting & Finance',
+    'data': [
+        './views/account_journal_dashboard_view.xml',
+    ],
+    'demo': [
+    ],
+    'depends': [
+        'public_budget',
+        'account_tax_settlement',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'name': 'Public Budget integration with Tax settlement',
+    'test': [],
+    'version': '9.0.1.0.0',
+}
