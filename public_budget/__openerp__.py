9b040cba8c8bebe583df5a297757849a0328b30d
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-06-27 16:31:36 -0300

    [ADD] add account user to view menu payments (#193)

--- public_budget/__openerp__.py
+++ public_budget/__openerp__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Public Budget',
     'license': 'AGPL-3',
-    'version': '9.0.1.26.0',
+    'version': '9.0.1.27.0',
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
     'category': 'Accounting & Finance',
