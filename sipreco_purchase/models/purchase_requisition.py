11960cea72ed55e855d062c570a07c83e32aa49d
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-11-20 15:50:58 -0300

    [IMP] some improvemnt to sipreco purchase (#213)
    * [IMP] new boolean field to set before confirm the requisition
    * [IMP] new orders and columns in the requisition tree view
    * [IMP] amount total for the requisition lines
    * [IMP] new filter and search fields in requisition model

--- sipreco_purchase/models/purchase_requisition.py
+++ sipreco_purchase/models/purchase_requisition.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from openerp.exceptions import ValidationError, UserError
+import openerp.addons.decimal_precision as dp
 # from dateutil.relativedelta import relativedelta
 import logging
 _logger = logging.getLogger(__name__)
@@ -32,11 +33,43 @@ class PurchaseRequisition(models.Model):
         # readonly=True,
         # states={'draft': [('readonly', False)]},
         domain="[('company_id', '=', company_id)]",
+        track_visibility='onchange',
     )
+
+    inspected = fields.Boolean(
+        'Inspected?'
+    )
+
+    amount_total = fields.Float(
+        digits=dp.get_precision('Product Price'),
+        compute='_compute_amount_total',
+    )
+
+    @api.depends('line_ids')
+    def _compute_amount_total(self):
+        for rec in self.filtered('line_ids'):
+            rec.amount_total = sum([x.subtotal for x in rec.line_ids])
+
+    @api.multi
+    def to_inspected(self):
+        if not self.type_id:
+            raise UserError(_('Antes de revisar debe tener establecido un'
+                              '"Tipo"'))
+        self.inspected = True
+
+    @api.multi
+    def to_draft(self):
+        if self.state == 'draft' and self.inspected:
+            self.inspected = False
+        elif self.state == 'in_progress':
+            self.with_context(cancel_procurement=False).tender_cancel()
+            self.tender_reset()
 
     @api.multi
     def tender_cancel(self):
-        self.mapped('manual_procurement_ids').button_cancel_remaining()
+        if self._context.get('cancel_procurement', True):
+            self.mapped('manual_procurement_ids').button_cancel_remaining()
+        self.inspected = False
         return super(PurchaseRequisition, self).tender_cancel()
 
     @api.multi
