51fd3a860b3f236da3b073b1ccf27371ec703f63
nicomacr <nmr@adhoc.com.ar>
2018-11-20 14:21:58 -0300

    [IMP] some improvemnt to sipreco purchase
    * [IMP] new boolean field to set before confirm the requisition
    * [IMP] new orders and columns in the requisition tree view
    * [IMP] amount total for the requisition lines
    * [IMP] new filter and search fields in requisition model

--- sipreco_purchase/models/purchase_requisition_line.py
+++ sipreco_purchase/models/purchase_requisition_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
 import openerp.addons.decimal_precision as dp
 # from openerp.exceptions import ValidationError
 # from dateutil.relativedelta import relativedelta
@@ -17,3 +17,13 @@ class PurchaseRequisitionLine(models.Model):
         string='Unit Price',
         digits=dp.get_precision('Product Price'),
     )
+    subtotal = fields.Float(
+        digits=dp.get_precision('Product Price'),
+        compute='_compute_subtotal',
+        store=True,
+    )
+
+    @api.depends('price_unit', 'product_qty')
+    def _compute_subtotal(self):
+        for rec in self:
+            rec.subtotal = rec.price_unit * rec.product_qty
