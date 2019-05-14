11a8456a376ca22bf92a20354d25af1f88eaefbf
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-05-14 15:39:05 -0300

    [IMP] public_budget: Implements improvement for assets (#289)

--- public_budget/wizards/__init__.py
+++ public_budget/wizards/__init__.py
@@ -4,3 +4,4 @@ from . import transaction_definitive_mass_invoice_create
 from . import budget_analysis_wizard
 from . import avance_request_report_wizard
 from . import public_budget_preventive_changeposition
+from . import transfer_assets_wizard
