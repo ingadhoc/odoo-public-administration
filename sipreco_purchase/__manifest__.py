04da9ccb359b7a5e319232372983fc6b361c187e
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-05-21 10:11:39 -0300

    [FIX] sipreco_purchase: set the requesition report xml to noupdate=true (#294)

--- sipreco_purchase/__manifest__.py
+++ sipreco_purchase/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sipreco Purchase Management',
-    'version': '11.0.1.8.0',
+    'version': '11.0.1.9.0',
     'license': 'AGPL-3',
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
