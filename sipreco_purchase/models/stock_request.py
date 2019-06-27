87894120c4a4cfacf96bdb2628a6d10a72070735
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-06-27 11:44:30 -0300

    [IMP] sipreco_purchase: fix the order of request
    Order request by 'create date' desc.

--- sipreco_purchase/models/stock_request.py
+++ sipreco_purchase/models/stock_request.py
@@ -9,6 +9,7 @@ from odoo.exceptions import ValidationError
 
 class StockRequest(models.Model):
     _inherit = 'stock.request'
+    _order = 'create_date desc'
 
     # odoo ya tiene un requisition_id que es cuando se correr el "buy", en este
     # caso es un link independiente a eso lo que queremos, un link manual y sin
