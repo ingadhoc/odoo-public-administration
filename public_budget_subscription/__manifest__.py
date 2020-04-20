765c89d5f153d055d31c9e51446943de214c08e3
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-04-20 10:21:49 -0300

    [MIG] public_budget_subscription: Migration to 13.0

--- public_budget_subscription/__manifest__.py
+++ public_budget_subscription/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Public Budget Subscription",
-    'version': '11.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Accounting',
     'sequence': 14,
     'summary': 'Subscription Purchase, Invoicing',
@@ -39,7 +39,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
