9d5e8af11270aba08aaf0111e8d2c5b9d25df960
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-05-22 09:41:38 -0300

    [11.0] [FIX] sipreco_purchase: fix some tracked problems in runbot

--- sipreco_purchase/__manifest__.py
+++ sipreco_purchase/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sipreco Purchase Management',
-    'version': '11.0.1.19.0',
+    'version': '11.0.1.20.0',
     'license': 'AGPL-3',
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
