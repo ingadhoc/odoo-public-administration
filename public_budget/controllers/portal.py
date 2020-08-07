bc0ed398a1c4caff749e0584ecec736389f40ce1
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-08-07 15:11:38 -0300

    [13.0] [FIX] public_budget: skip the validation of users gruops for porta users.
    To avoid this newly restriction https://github.com/odoo/odoo/commit/29cbba73a71990e76f497ee892ce16fc7fc53d9f
    For portal user we need the access to backend.

--- public_budget/controllers/portal.py
+++ public_budget/controllers/portal.py
@@ -1,5 +1,7 @@
 from odoo import http
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.portal.controllers.web import Home as PortalHome
+from odoo.addons.web.controllers.main import Home
 from odoo.http import request
 
 
@@ -9,3 +11,13 @@ class CustomerPortal(CustomerPortal):
     def home(self, **kw):
         # Anulamos portal para este caso que simpre redirija a backend
         return request.redirect('/web')
+
+
+class PortalHome(PortalHome):
+
+    @http.route()
+    def web_client(self, s_action=None, **kw):
+        if request.session.uid and request.env['res.users'].sudo().browse(
+                request.session.uid).has_group('base.group_portal'):
+            return Home.web_client(self, s_action=s_action, **kw)
+        return super(PortalHome, self).web_client(s_action, **kw)
