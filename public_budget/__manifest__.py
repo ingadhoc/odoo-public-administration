fad3c0d4173983e976085ef03502388d3db52547
Maximiliano Mezzavilla <mem@adhoc.com>
2023-03-14 18:00:37 +0000

    [FIX] public_budget: Se muestra en en la vista de trasnferencias el campo Fecha
    closes ingadhoc/odoo-public-administration#476
    
    Ticket: 59996
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- public_budget/__manifest__.py
+++ public_budget/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Public Budget',
     'license': 'AGPL-3',
-    'version': "15.0.1.4.0",
+    'version': "15.0.1.5.0",
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
     'category': 'Accounting & Finance',
