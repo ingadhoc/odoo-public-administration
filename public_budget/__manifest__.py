8a4a4bf60b3ab4c191d4da82913ad37c7d7d9ebc
Pablo Montenegro <pam@adhoc.com.ar>
2023-05-16 18:08:18 +0000

    [FIX] public_budget: permitir visualizar transacciones a usuario portal
    closes ingadhoc/odoo-public-administration#485
    
    Ticket: 61849
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- public_budget/__manifest__.py
+++ public_budget/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Public Budget',
     'license': 'AGPL-3',
-    'version': "15.0.1.6.0",
+    'version': "15.0.1.7.0",
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
     'category': 'Accounting & Finance',
