dec1874255e5ad6c4abec89980295fb91a8fea2a
Ignacio Ramirez <iga@adhoc.com.ar>
2025-04-16 14:41:53 -0300

    [MIG] public_budget_tax_settlement: Migration to 18.0

--- public_budget_tax_settlement/models/account_move_line.py
+++ public_budget_tax_settlement/models/account_move_line.py
@@ -5,5 +5,11 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     payment_methods = fields.Char(
-        related='payment_id.payment_group_id.payment_methods',
+        # related='payment_id.payment_group_id.payment_methods',
+        compute="_compute_payment_methods",
     )
+
+
+    def _compute_payment_methods(self):
+        for rec in self:
+            rec.payment_methods = ', '.join(rec.payment_ids.mapped('payment_method')) if rec.payment_ids else False
