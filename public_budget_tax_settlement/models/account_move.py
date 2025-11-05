d08dca9019aafa5876c4f9814e0cb0268eae8e1e
Juan José Scarafia <jjs@adhoc.inc>
2025-11-05 18:23:48 +0000

    [FIX] tax settlement
    X-original-commit: 6b7625f9d5f5cbe9b67b2b16927d53f5977e5fde

--- public_budget_tax_settlement/models/account_move.py
+++ public_budget_tax_settlement/models/account_move.py
@@ -10,42 +10,16 @@ class AccoutMove(models.Model):
     )
 
     def action_pay_tax_settlement(self):
-        self.ensure_one()
-        open_move_line_ids = self.line_ids.filtered(
-            lambda r: not r.reconciled and r.account_id.internal_type in (
-                'payable', 'receivable'))
-        return {
-            'name': _('Register Payment'),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'account.payment.group',
-            'view_id': False,
-            'target': 'current',
-            'type': 'ir.actions.act_window',
-            'context': {
-                'to_pay_move_line_ids': open_move_line_ids.ids,
-                'pop_up': True,
-                'default_company_id': self.company_id.id,
-                'default_partner_id': self.partner_id.id,
-                'default_partner_type': 'supplier',
-                # por defecto, en pago de retenciones, no hacemos double
-                # validation
-                'force_simple': True,
-                # evitamos la restricion de que solo podes agregar pagos en facturas validadas
-                'active_model': 'account.payment',
-            },
-        }
+        return self.settled_line_ids.action_pay_tax_settlement()
 
     def _compute_matched_to_pay(self):
         for rec in self:
-            if rec.line_ids and not rec.payment_state and rec.state == 'posted' and rec.open_move_line_ids:
-                amount_residual = sum(rec.line_ids.mapped('amount_residual'))
-                if amount_residual != 0:
-                    rec.amount_residual = amount_residual
-            rec.enable_to_pay = True if rec.amount_residual != 0 else False
+            rec.enable_to_pay = any(x.tax_state == 'to_pay' for x in rec.settled_line_ids)
 
     @api.model
     def create(self, values):
+        # aparentemente esto lo pusimos para que cuando se crea el asiento de liquidación el asiento quede
+        # con partner definido
         res = super().create(values)
         if res.move_type == 'entry' and not res.partner_id and len(res.line_ids.mapped('partner_id')) == 1:
             res.partner_id = res.line_ids.mapped('partner_id')
