f522a8f10268cbe375bdf4e0fa7e3c1d689d2273
Ignacio Ramirez <iga@adhoc.com.ar>
2026-03-03 13:05:27 -0300

    [IMP] public_budget: Fix some views and recover funcionality for payments task: 63725

--- sipreco_purchase/models/purchase_order_line.py
+++ sipreco_purchase/models/purchase_order_line.py
@@ -10,11 +10,3 @@ class PurchaseOrderLine(models.Model):
 
     brand = fields.Char(
     )
-
-    @api.depends('product_qty', 'product_uom', 'company_id', 'order_id.partner_id')
-    def _compute_price_unit_and_date_planned_and_name(self):
-        price_no_update_lines = self.filtered(
-            lambda line: line.order_id.requisition_id
-        )
-        res = super(PurchaseOrderLine, self - price_no_update_lines)._compute_price_unit_and_date_planned_and_name()
-        return res
